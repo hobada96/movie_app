72a694bcb51930bbdf300f536e20391bd7c95a9f
hobada96 <hobada96@gmail.com>
2021-02-06 14:29:30 +0900

    add React class component example with life cycle and state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-const foodLike = [
-    {
-        id : 1,
-        name : "kimbap"},
-    {
-        id : 2,
-        name : "sushi"},
-    {
-        id : 3,
-        name : "bread"}
-]
-
-const Food = ({name}) =>{
-    return(
-        <div>
-            <h2>i like {name}</h2>
-        </div>
-    )
-}
-function App() {
-  return (
-    <div className="App">
-        {foodLike.map(dish => (
-            <Food key = {dish.id} name={dish.name}></Food>
-        ))}
-
-    </div>
-  );
+import React from 'react';
+class App extends React.Component{
+    state = {
+        isLoading: true,
+        movies: []
+    };
+    componentDidMount() {
+        setTimeout(()=> {
+            this.setState({isLoading: false});
+        }, 6000);
+    }
+    render() {
+        const {isLoading} = this.state;
+        return <div>{isLoading ? "Loading...": "we are ready"}</div>
+    }
 }
 
 export default App;
