3b2fa28ce6e810ac691b5b0fe4620c5bbacd7fdd
hobada96 <hobada96@gmail.com>
2021-02-07 16:58:17 +0900

    get Movies from yts.mx/api and show movies in page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React,{useState,useEffect} from 'react';
-const App = () =>{
-    const [Loading,isLoading] = useState(true);
-    useEffect(()=>{
-        setTimeout(()=>{
-            isLoading(!Loading)
-        }, 6000)
-    })
-    return(
-        <div>{Loading ? "Loading...": "we are ready"}</div>
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+
+const App = () => {
+    const [movies, setMovies] = useState(null);
+    const [Loading, isLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                setError(false)
+                setMovies(null);
+                const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json");
+                setMovies(movies);
+            } catch (e) {
+                setError(e)
+            }
+            isLoading(false);
+        };
+        fetchMovies();
+    },[]) // 여기서 에러 발생.
+
+    if(error) return <div>에러발생</div>
+    return (
+            <div>{Loading
+                ? "Loading..."
+                : movies.map(movie => {
+                return <Movie
+                         id={movie.id}
+                         year={movie.year}
+                         title={movie.title}
+                         summary={movie.summary}
+                         poster={movie.poster} />
+            })}</div>
     );
 }
 
 export default App;
+
