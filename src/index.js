c2f98774d42544a5ddf162f03ad2e0b7a7c129de
hobada96 <hobada96@gmail.com>
2021-03-06 23:46:52 +0900

    add redux with error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import movieRedux from './redux'
+import { Provider } from 'react-redux';
+import createStore from 'redux'
+
+const store = createStore(movieRedux)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
