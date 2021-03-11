8348fd5d1b20db428799b8462cd34c429ee2c762
hobada96 <hobada96@gmail.com>
2021-03-11 21:52:20 +0900

    use reduce and custom hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import movieRedux from './redux'
-import { Provider } from 'react-redux';
-import createStore from 'redux'
-
-const store = createStore(movieRedux)
 
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
           <App />
-      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
