06e93f1b135a1f39a6bc5e5374ef1c5304b0c126
hobada96 <hobada96@gmail.com>
2021-03-12 16:16:51 +0900

    redux 중간 저장

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux'
+import rootReducer from './modules/rootReducer'
+import {createStore} from "redux";
+import Reactotron from 'reactotron-react-js'
 
+const store = createStore(rootReducer);
+Reactotron.configure().connect();
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
           <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
---
+++ src/modules/rootReducer.js
@@ -0,0 +1,1 @@
+import {combineReducers} from "redux";import movies from './movies'const rootReducer = combineReducers({        movies    })export default rootReducer;
